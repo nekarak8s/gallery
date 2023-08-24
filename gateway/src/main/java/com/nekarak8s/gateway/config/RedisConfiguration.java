ea54e05195b671e41317871e9a109a34acd49170
dojsfffff <dojsfffff@naver.com>
2023-08-24 21:13:18 +0900

    BE:chore 레디스 비밀번호 설정 추가

--- gateway/src/main/java/com/nekarak8s/gateway/config/RedisConfiguration.java
+++ gateway/src/main/java/com/nekarak8s/gateway/config/RedisConfiguration.java
@@ -1,13 +1,16 @@
 package com.nekarak8s.gateway.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisPassword;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
 @Configuration
+@Slf4j
 public class RedisConfiguration {
 
     @Value("${spring.redis.host}")
@@ -16,9 +19,21 @@ public class RedisConfiguration {
     @Value("${spring.redis.port}")
     private int port;
 
+    @Value("${spring.redis.password}")
+    private String password;
+
+//    @Bean
+//    public RedisConnectionFactory redisConnectionFactory() {
+//        LettuceConnectionFactory factory = new LettuceConnectionFactory(host, port);
+//        factory.setPassword(password); // deprecated
+//        log.info("pass : {}", factory.getPassword());
+//        return factory;
+//    }
     @Bean
-    public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(host, port);
+    public LettuceConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration redisConfig = new RedisStandaloneConfiguration(host, port);
+        redisConfig.setPassword(RedisPassword.of(password));
+        return new LettuceConnectionFactory(redisConfig);
     }
 
     @Bean
