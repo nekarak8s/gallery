acb79a4a9fb3820dbff4ca223a978ac29a5e04e5
Byongho96 <unlike96@gmail.com>
2023-08-26 01:55:44 +0900

    FE:feat: update home-section-1 component
    - update the image sources
    - change png file to webp for lower data size

--- frontend/src/pages/Home/HomeSection1/HomeSection1.tsx
+++ frontend/src/pages/Home/HomeSection1/HomeSection1.tsx
@@ -1,11 +1,12 @@
 import React, { useEffect } from 'react'
 import './HomeSection1.scss'
 
-import skyImg from '@/assets/images/home-section-1/sky.png'
-import forestImg from '@/assets/images/home-section-1/forest.png'
-import architectureImg from '@/assets/images/home-section-1/architecture.png'
-import leftTreeImg from '@/assets/images/home-section-1/left-tree.png'
-import rightTreeImg from '@/assets/images/home-section-1/right-tree.png'
+import skyImg from '@/assets/images/home-section-1/sky.webp'
+import forestImg from '@/assets/images/home-section-1/forest.webp'
+import architectureImg from '@/assets/images/home-section-1/architecture.webp'
+import reflectionImg from '@/assets/images/home-section-1/reflection.webp'
+import leftTreeImg from '@/assets/images/home-section-1/left-tree.webp'
+import rightTreeImg from '@/assets/images/home-section-1/right-tree.webp'
 import { throttle } from 'lodash'
 
 function HomeSection1() {
@@ -24,18 +25,17 @@ function HomeSection1() {
         const speedx = Number(el.dataset.speedx)
         const speedy = Number(el.dataset.speedy)
         const speedz = Number(el.dataset.speedz)
-        const rotation = Number(el.dataset.rotation)
+        const rotation = Number(el.dataset.rotation) * 2
+        const layer = Number(el.dataset.layer) * 100
 
         const leftX = parseFloat(getComputedStyle(el).left)
         const isInLeft = leftX < window.innerHeight / 2 ? 1 : -1
         const zValue = e.clientX - leftX * isInLeft * 0.1
 
         // console.log(xValue, yValue)
-        el.style.transform = `rotateY(${
-          rotateDeg * rotation * 2
-        }deg) translate3d(${xValue * speedx}px, ${yValue * speedy}px, ${
-          zValue * speedz
-        }px)`
+        el.style.transform = `rotateY(${rotateDeg * rotation}deg) translate3d(${
+          xValue * speedx
+        }px, ${yValue * speedy}px, ${zValue * speedz + layer}px)`
       })
     }
 
@@ -54,51 +54,66 @@ function HomeSection1() {
         <div className="vignette"></div>
         <img
           className="home-section-1__sky-img parallax"
-          data-speedx="0.3"
-          data-speedy="0.3"
-          data-speedz="0.3"
-          data-rotation="0.3"
+          data-speedx="0.2"
+          data-speedy="0.2"
+          data-speedz="0"
+          data-rotation="0"
+          data-layer="1"
           src={skyImg}
         ></img>
         <img
           className="home-section-1__forest-img parallax"
-          data-speedx="0.25"
-          data-speedy="0.25"
-          data-speedz="0.25"
-          data-rotation="0.25"
+          data-speedx="0.15"
+          data-speedy="0.15"
+          data-speedz="0"
+          data-rotation="0.1"
+          data-layer="2"
           src={forestImg}
         ></img>
         <div
           className="home-section-1__gallery-logo parallax"
-          data-speedx="0.25"
-          data-speedy="0.25"
-          data-speedz="0.25"
-          data-rotation="0.25"
+          data-speedx="0.12"
+          data-speedy="0.12"
+          data-speedz="0"
+          data-rotation="0.1"
+          data-layer="3"
         >
           Gallery
         </div>
         <img
           className="home-section-1__architecture-img parallax"
-          data-speedx="0.2"
-          data-speedy="0.2"
-          data-speedz="0.2"
-          data-rotation="0.2"
+          data-speedx="0.1"
+          data-speedy="0.1"
+          data-speedz="0"
+          data-rotation="0.1"
+          data-layer="4"
           src={architectureImg}
         ></img>
         <img
-          className="home-section-1__left-tree-img parallax"
+          className="home-section-1__reflection-img parallax"
           data-speedx="0.1"
           data-speedy="0.1"
-          data-speedz="0.1"
+          data-speedz="0"
           data-rotation="0.1"
+          data-layer="5"
+          src={reflectionImg}
+        ></img>
+        <img
+          className="home-section-1__left-tree-img parallax"
+          data-speedx="0.05"
+          data-speedy="0.05"
+          data-speedz="0.05"
+          data-rotation="0.1"
+          data-layer="6"
           src={leftTreeImg}
         ></img>
         <img
           className="home-section-1__right-tree-img parallax"
-          data-speedx="0.1"
-          data-speedy="0.1"
-          data-speedz="0.1"
+          data-speedx="0.05"
+          data-speedy="0.05"
+          data-speedz="0.05"
           data-rotation="0.1"
+          data-layer="7"
           src={rightTreeImg}
         ></img>
       </div>
