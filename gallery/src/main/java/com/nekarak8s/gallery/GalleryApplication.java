474d91fc7092c7c3dca2790cdc1fe2b0099bcfa2
dojsfffff <dojsfffff@naver.com>
2023-08-31 21:05:01 +0900

    BE:init 프로젝트 초기 설정

---
+++ gallery/src/main/java/com/nekarak8s/gallery/GalleryApplication.java
@@ -0,0 +1,13 @@
+package com.nekarak8s.gallery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GalleryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GalleryApplication.class, args);
+    }
+
+}
