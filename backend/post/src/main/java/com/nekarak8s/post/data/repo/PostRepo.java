95e6392ee7fea635d4d371096f9fd49a005b8ba7
dojsfffff <dojsfffff@naver.com>
2023-11-25 17:35:29 +0900

    BE:feat: delete dumy post 10 by gallery

--- backend/post/src/main/java/com/nekarak8s/post/data/repo/PostRepo.java
+++ backend/post/src/main/java/com/nekarak8s/post/data/repo/PostRepo.java
@@ -2,8 +2,16 @@ package com.nekarak8s.post.data.repo;
 
 import com.nekarak8s.post.data.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PostRepo extends JpaRepository<Post, Long> {
+
+    // 게시물 삭제
+    @Modifying
+    @Query("DELETE FROM Post p WHERE p.galleryId = :galleryId")
+    void deleteAllByGalleryId(@Param("galleryId") long galleryId);
 }
--- backend/post/src/main/java/com/nekarak8s/post/service/PostService.java
+++ backend/post/src/main/java/com/nekarak8s/post/service/PostService.java
@@ -8,6 +8,11 @@ public interface PostService {
     /**
      * 갤러리 생성에 따른 게시물 생성
      */
-    void createPost(long galleryId, int count);
+    void createPostByGallery(long galleryId, int count);
+
+    /**
+     * 갤러리 삭제에 따른 게시물 삭제
+     */
+    void deletePostByGallery(long galleryId);
 
 }
