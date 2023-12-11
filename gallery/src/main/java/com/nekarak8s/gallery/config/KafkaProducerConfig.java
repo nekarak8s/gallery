e7ac7b5f81c6952f74de79aacbb207f4819ba9fe
dojsfffff <dojsfffff@naver.com>
2023-12-11 20:24:56 +0900

    BE:feat: send gallery create & delete event to kafka

--- gallery/src/main/java/com/nekarak8s/gallery/config/KafkaProducerConfig.java
+++ gallery/src/main/java/com/nekarak8s/gallery/config/KafkaProducerConfig.java
@@ -18,18 +18,18 @@ public class KafkaProducerConfig {
     private String bootstrapServers;
 
     @Bean
-    public ProducerFactory<String, String> producerFactory() {
+    public ProducerFactory<String, Object> producerFactory() {
         Map<String, Object> configProps = new HashMap<>();
         configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, org.apache.kafka.common.serialization.StringSerializer.class);
-        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, org.apache.kafka.common.serialization.StringSerializer.class);
-        configProps.put(ProducerConfig.RETRIES_CONFIG, 3);
-        configProps.put(ProducerConfig.MAX_BLOCK_MS_CONFIG, 5000);
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, org.springframework.kafka.support.serializer.JsonSerializer.class);
+        configProps.put(ProducerConfig.RETRIES_CONFIG, 3); // 재시도
+        configProps.put(ProducerConfig.ACKS_CONFIG, "all"); // 0, -1, all
         return new DefaultKafkaProducerFactory<>(configProps);
     }
 
     @Bean
-    public KafkaTemplate<String, String> kafkaTemplate() {
+    public KafkaTemplate<String, Object> kafkaTemplate() {
         return new KafkaTemplate<>(producerFactory());
     }
 }
