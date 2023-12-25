f18c61ee6066eda7adb867beab331105c748a531
dojsfffff <dojsfffff@naver.com>
2023-12-25 11:25:49 +0900

    BE:refactor: social login

--- backend/member/src/main/java/com/nekarak8s/member/service/AuthService.java
+++ backend/member/src/main/java/com/nekarak8s/member/service/AuthService.java
@@ -3,16 +3,12 @@ package com.nekarak8s.member.service;
 import com.nekarak8s.member.exception.CustomException;
 import org.springframework.stereotype.Service;
 
-import java.util.Map;
-
 @Service
 public interface AuthService {
 
     String getAuthorizationUrl();
 
     String getAccessToken(String code) throws CustomException;
-
-    Map getOAuthMemberInfo(String token);
 
     String getOAuthNickname(String token);
 
--- backend/member/src/main/java/com/nekarak8s/member/service/impl/AuthServiceImpl.java
+++ backend/member/src/main/java/com/nekarak8s/member/service/impl/AuthServiceImpl.java
@@ -2,6 +2,7 @@ package com.nekarak8s.member.service.impl;
 
 import com.nekarak8s.member.exception.CustomException;
 import com.nekarak8s.member.service.AuthService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
@@ -14,6 +15,7 @@ import java.net.URI;
 import java.util.Map;
 
 @Slf4j
+@RequiredArgsConstructor
 @Service
 public class AuthServiceImpl implements AuthService {
     @Value("${kakao.client-id}")
@@ -21,8 +23,16 @@ public class AuthServiceImpl implements AuthService {
     @Value("${kakao.redirect-uri}")
     private String redirectUri;
 
+    private static final String KAKAO_TOKEN_REQUEST_URL = "https://kauth.kakao.com/oauth/token";
     private static final String KAKAO_USER_INFO_URL = "https://kapi.kakao.com/v2/user/me";
 
+    // util
+    private final RestTemplate restTemplate;
+
+    /**
+     * 소셜 로그인 리다이렉트 URL 생성
+     * @return URL
+     */
     @Override
     public String getAuthorizationUrl() {
         StringBuffer sb = new StringBuffer();
@@ -35,71 +45,90 @@ public class AuthServiceImpl implements AuthService {
                 .toString();
     }
 
+    /**
+     * 소셜 로그인 엑세스 토큰 반환
+     * @param code
+     * @return token
+     */
     @Override
     public String getAccessToken(String code) throws CustomException {
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+        URI tokenURI = buildTokenURI(code);
+        
+        try {
+            ResponseEntity<Map> response = restTemplate.getForEntity(tokenURI, Map.class);
+            return extractAccessToken(response.getBody());
+        } catch (HttpClientErrorException e) {
+            handleHttpClientErrorException(e);
+        } catch (Exception e) {
+            handleGeneralException(e);
+        }
 
-        URI tokenUri = UriComponentsBuilder.fromHttpUrl("https://kauth.kakao.com/oauth/token")
+        throw new CustomException(HttpStatus.INTERNAL_SERVER_ERROR, "GA001", "액세스 토큰을 가져오는 데 실패했습니다");
+    }
+
+    private URI buildTokenURI(String code) {
+        return UriComponentsBuilder.fromHttpUrl(KAKAO_TOKEN_REQUEST_URL)
                 .queryParam("grant_type", "authorization_code")
                 .queryParam("code", code)
                 .queryParam("client_id", clientId)
                 .queryParam("redirect_uri", redirectUri)
                 .build()
                 .toUri();
-
-        RestTemplate restTemplate = new RestTemplate();
-
-        try {
-            ResponseEntity<Map> response = restTemplate.postForEntity(tokenUri, null, Map.class);
-
-            if (response.getStatusCode().is2xxSuccessful()) {
-                return (String) response.getBody().get("access_token");
-            } else {
-                throw new RuntimeException("서버 통신 에러");
-            }
-        } catch (HttpClientErrorException e) {
-            log.debug("유효하지 않은 인가코드");
-            throw new CustomException(HttpStatus.BAD_REQUEST, "GA005" , "유효하지 않은 인가 코드 입니다");
-        } catch (Exception e) {
-            log.debug("서버 에러");
-            throw new RuntimeException("서버 에러");
-        }
     }
 
-    @Override
-    public Map getOAuthMemberInfo(String token) {
-
-        // 회원 정보 조회 요청을 위한 설정
-        HttpHeaders headers = new HttpHeaders();
-        headers.setBearerAuth(token);
-        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-
-        RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<Map> userInfoResponse = restTemplate.exchange(KAKAO_USER_INFO_URL, HttpMethod.GET, new HttpEntity<>(headers), Map.class);
-
-        if (userInfoResponse.getStatusCode().is2xxSuccessful()) {
-            return userInfoResponse.getBody();
-        } else {
-            throw new RuntimeException("카카오 서버 통신 에러");
-        }
+    private void handleHttpClientErrorException(HttpClientErrorException e) throws CustomException {
+        log.debug("유효하지 않은 인가코드");
+        throw new CustomException(HttpStatus.BAD_REQUEST, "GA005", "유효하지 않은 인가 코드");
     }
 
+    private void handleGeneralException(Exception e) throws CustomException {
+        log.debug("서버 에러");
+        throw new CustomException(HttpStatus.INTERNAL_SERVER_ERROR, "GA001", "서버 에러");
+    }
+
+    private String extractAccessToken(Map<String, Object> responseBody) {
+        return (String) responseBody.get("access_token");
+    }
+
+    /**
+     * 소셜 로그인 닉네임 조회
+     * @param token
+     * @return
+     */
     @Override
     public String getOAuthNickname(String token) {
         Map kakaoUserInfo = getOAuthMemberInfo(token);
         Map kakaoAccount = (Map) kakaoUserInfo.get("kakao_account");
         Map kakaoProfile = (Map) kakaoAccount.get("profile");
-        String kakaoNickname = (String) kakaoProfile.get("nickname");
-        return kakaoNickname;
+        return (String) kakaoProfile.get("nickname");
     }
 
+    /**
+     * 소셜 로그인 아이디 조회
+     * @param token
+     * @return
+     */
     @Override
     public long getOAuthId(String token) {
         Map kakaoUserInfo = getOAuthMemberInfo(token);
-        long kakaoId = (long) kakaoUserInfo.get("id");
-        return kakaoId;
+        return (long) kakaoUserInfo.get("id");
     }
 
+    private Map getOAuthMemberInfo(String token) {
+        HttpHeaders headers = createKakaoOAuthHeaders(token);
+        ResponseEntity<Map> response = restTemplate.exchange(
+                KAKAO_USER_INFO_URL,
+                HttpMethod.GET,
+                new HttpEntity<>(headers),
+                Map.class);
 
+        return response.getBody();
+    }
+
+    private HttpHeaders createKakaoOAuthHeaders(String token) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBearerAuth(token);
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+        return headers;
+    }
 }
