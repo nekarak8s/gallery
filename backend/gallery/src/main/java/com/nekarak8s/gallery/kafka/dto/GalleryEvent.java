5d2c242d0bbbb94f2510afad51159f89bd025d02
dojsfffff <dojsfffff@naver.com>
2023-12-26 09:54:39 +0900

    BE:refactor: gallery creation service

--- backend/gallery/src/main/java/com/nekarak8s/gallery/kafka/dto/GalleryEvent.java
+++ backend/gallery/src/main/java/com/nekarak8s/gallery/kafka/dto/GalleryEvent.java
@@ -1,13 +1,22 @@
 package com.nekarak8s.gallery.kafka.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class GalleryEvent {
     private long galleryId;
     private String type;
+
+    public static GalleryEvent createGalleryEvent(long galleryId, String type) {
+        return GalleryEvent.builder()
+                .galleryId(galleryId)
+                .type(type)
+                .build();
+    }
 }
