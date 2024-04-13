a7d4d4a7b87732b974b29077aba7f3b1292c122a
Byongho <unlike96@gmail.com>
2024-04-13 19:11:24 +0900

    FE:feat: remove shoot button of joystick

--- frontend/src/atoms/ui/Joystick/Joystick.tsx
+++ frontend/src/atoms/ui/Joystick/Joystick.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useRef } from 'react'
-import BowIcon from '@/assets/svgs/bow.svg'
 import JumpIcon from '@/assets/svgs/jump.svg'
 import toFrame from '@/utils/toFrame'
 import './Joystick.scss'
@@ -180,14 +179,10 @@ const Joystick = ({ control, shoot, jump }: JoystickProps) => {
       <div className="joystick__controller" ref={joystickRef}>
         <div className="joystick__controller--core" ref={coreRef}></div>
       </div>
-      <div className="joystick__buttons">
-        <div className="joystick__shoot" ref={shootRef}>
-          <BowIcon />
-        </div>
-        <div className="joystick__jump" ref={jumpRef}>
-          <JumpIcon />
-        </div>
+      <div className="joystick__jump" ref={jumpRef}>
+        <JumpIcon />
       </div>
+      <div className="joystick__shoot" ref={shootRef}></div>
     </div>
   )
 }
