3863053eb7073e4b603fa71ecf76bca649a19f39
Byongho <unlike96@gmail.com>
2024-04-13 21:55:12 +0900

    FE:chore: update CSS styles

--- frontend/src/features/gallery/services.ts
+++ frontend/src/features/gallery/services.ts
@@ -6,38 +6,31 @@ import axiosInstance from '@/utils/axiosInstance'
 import toastManager from '@/utils/toastManager/toastManager'
 
 export function useGalleryListQuery(options?: TUseQueryOptions<GalleryData[]>) {
-  return useQuery<MessageResponse<GalleryData[]>, ErrorResponse, GalleryData[]>(
-    ['gallery-list'],
-    () => axiosInstance.get(`/gallery/list`),
-    {
-      onSuccess: () => {},
-      onError: (err) => {
-        toastManager.addToast('error', err.message)
-      },
-      select: (res) => {
-        return res.data
-      },
-      staleTime: Infinity,
-      ...options,
-    }
-  )
+  return useQuery<MessageResponse<GalleryData[]>, ErrorResponse, GalleryData[]>(['gallery-list'], () => axiosInstance.get(`/gallery/list`), {
+    onSuccess: () => {},
+    onError: (err) => {
+      toastManager.addToast('error', err.message)
+    },
+    select: (res) => {
+      return res.data
+    },
+    staleTime: Infinity,
+    ...options,
+  })
 }
 
 export function useCreateGallery(options?: TUseMutationOptions<GalleryFormData>) {
   const queryClient = useQueryClient()
-  return useMutation<MessageResponse, ErrorResponse, GalleryFormData>(
-    (data) => axiosInstance.post(`/gallery`, data),
-    {
-      onSuccess: (res) => {
-        queryClient.invalidateQueries(['gallery-list'])
-        toastManager.addToast('success', res.message)
-      },
-      onError: (err) => {
-        toastManager.addToast('error', err.message)
-      },
-      ...options,
-    }
-  )
+  return useMutation<MessageResponse, ErrorResponse, GalleryFormData>((data) => axiosInstance.post(`/gallery`, data), {
+    onSuccess: (res) => {
+      queryClient.invalidateQueries(['gallery-list'])
+      toastManager.addToast('success', res.message)
+    },
+    onError: (err) => {
+      toastManager.addToast('error', err.message)
+    },
+    ...options,
+  })
 }
 
 export function useGalleryQuery(galleryId: number, options?: TUseQueryOptions<GalleryData>) {
@@ -58,50 +51,37 @@ export function useGalleryQuery(galleryId: number, options?: TUseQueryOptions<Ga
   )
 }
 
-export function useUpdateGallery(
-  galleryId: number,
-  options?: TUseMutationOptions<GalleryFormData>
-) {
+export function useUpdateGallery(galleryId: number, options?: TUseMutationOptions<GalleryFormData>) {
   const queryClient = useQueryClient()
-  return useMutation<MessageResponse, ErrorResponse, GalleryFormData>(
-    (data) => axiosInstance.patch(`/gallery/${galleryId}`, data),
-    {
-      onSuccess: (res) => {
-        queryClient.invalidateQueries(['gallery-list'])
-        queryClient.invalidateQueries(['gallery', { galleryId }])
-        toastManager.addToast('success', res.message)
-      },
-      onError: (err) => {
-        toastManager.addToast('error', err.message)
-      },
-      ...options,
-    }
-  )
+  return useMutation<MessageResponse, ErrorResponse, GalleryFormData>((data) => axiosInstance.patch(`/gallery/${galleryId}`, data), {
+    onSuccess: (res) => {
+      queryClient.invalidateQueries(['gallery-list'])
+      queryClient.invalidateQueries(['gallery', { galleryId }])
+      toastManager.addToast('success', res.message)
+    },
+    onError: (err) => {
+      toastManager.addToast('error', err.message)
+    },
+    ...options,
+  })
 }
 
 export function useDeleteGallery(galleryId: number, options?: TUseMutationOptions) {
   const queryClient = useQueryClient()
-  return useMutation<MessageResponse, ErrorResponse>(
-    () => axiosInstance.delete(`/gallery/${galleryId}`),
-    {
-      onSuccess: (res) => {
-        queryClient.invalidateQueries(['gallery-list'])
-        queryClient.invalidateQueries(['gallery', { galleryId }])
-        toastManager.addToast('success', res.message)
-      },
-      onError: (err) => {
-        toastManager.addToast('error', err.message)
-      },
-      ...options,
-    }
-  )
+  return useMutation<MessageResponse, ErrorResponse>(() => axiosInstance.delete(`/gallery/${galleryId}`), {
+    onSuccess: (res) => {
+      queryClient.invalidateQueries(['gallery-list'])
+      queryClient.invalidateQueries(['gallery', { galleryId }])
+      toastManager.addToast('success', res.message)
+    },
+    onError: (err) => {
+      toastManager.addToast('error', err.message)
+    },
+    ...options,
+  })
 }
 
-export function useSearchGalleryQuery(
-  type: string,
-  query: string,
-  options?: TUseQueryOptions<GallerySearchItemData[]>
-) {
+export function useSearchGalleryQuery(type: string, query: string, options?: TUseQueryOptions<GallerySearchItemData[]>) {
   return useQuery<MessageResponse<GallerySearchItemData[]>, ErrorResponse, GallerySearchItemData[]>(
     ['gallery', { type, query }],
     () =>
@@ -109,6 +89,7 @@ export function useSearchGalleryQuery(
         params: {
           type,
           query,
+          size: 5,
         },
       }),
     {
@@ -126,19 +107,15 @@ export function useSearchGalleryQuery(
 }
 
 export function usePlaceListQuery(options?: TUseQueryOptions<PlaceData[]>) {
-  return useQuery<MessageResponse<PlaceData[]>, ErrorResponse, PlaceData[]>(
-    ['place'],
-    () => axiosInstance.get(`/gallery/place/list`),
-    {
-      onSuccess: () => {},
-      onError: (err) => {
-        toastManager.addToast('error', err.message)
-      },
-      select: (res) => {
-        return res.data
-      },
-      staleTime: Infinity,
-      ...options,
-    }
-  )
+  return useQuery<MessageResponse<PlaceData[]>, ErrorResponse, PlaceData[]>(['place'], () => axiosInstance.get(`/gallery/place/list`), {
+    onSuccess: () => {},
+    onError: (err) => {
+      toastManager.addToast('error', err.message)
+    },
+    select: (res) => {
+      return res.data
+    },
+    staleTime: Infinity,
+    ...options,
+  })
 }
